90624106604250e94d1c29139625d10c3f40ae2c
Michael Kelleher <kelleher.mike@gmail.com>
2023-04-20 15:04:04 -0600

    Add Cheyenne setup script

---
+++ setup_cheyenne.bash
@@ -0,0 +1,12 @@
+module purge
+module load ncarenv/1.2
+module load intel/17.0.1
+module load mkl/2017.0.1
+module load mpt/2.15f
+module load netcdf-mpi/4.4.1.1
+module load ncarcompilers/0.4.1
+module load pnetcdf/1.8.0
+module load cmake/3.7.2
+module load conda/latest
+
+module -t list
